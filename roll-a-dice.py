863f69ab7f0b111e8c9717a6c129e209c1c304d4
ZinyCrying <112892791+ZinyCrying@users.noreply.github.com>
2023-03-14 21:29:18 +0530

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,53 @@
+import random
+
+response = "y"
+
+while response == "y":
+    no = random.randint(1,6)
+    if no == 1:
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
+    
+    elif no == 2:
+        print("[-----]")
+        print("[     ]")
+        print("[  00 ]")
+        print("[     ]")
+        print("[-----]")
+    
+    elif no == 3:
+        print("[-----]")
+        print("[     ]")
+        print("[ 000 ]")
+        print("[     ]")
+        print("[-----]")
+    
+    elif no == 4:
+        print("[-----]")
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+        print("[-----]")
+
+    elif no == 5:
+        print("[-----]")
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+        print("[-----]")
+    
+    elif no == 6:
+        print("[-----]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[-----]")
+
+    response = input("press 'y' to continue and 'n' for exit: ")
+    
+
+    
+    
